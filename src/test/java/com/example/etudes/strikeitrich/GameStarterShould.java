04277d39fd997e2c47059764906686e247170d9f
Alvaro Garcia <derecerca@gmail.com>
2016-03-19 15:40:11 +0100

    refactor: AAA in tests

--- src/test/java/com/example/etudes/strikeitrich/GameStarterShould.java
+++ src/test/java/com/example/etudes/strikeitrich/GameStarterShould.java
@@ -10,8 +10,8 @@ public class GameStarterShould {
     @Test
     public void deal_to_players() throws Exception {
         Player player1 = new Player(0, 0, 0, 0);
-
         GameStarter gameStarter = new GameStarter();
+
         gameStarter.deal(player1);
 
         assertThat(player1, is(new Player(2, 4, 2, 10_000)));

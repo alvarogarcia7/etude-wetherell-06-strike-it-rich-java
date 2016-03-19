1f147b061814dd5063b7c98d66ae39aa3072c2aa
Alvaro Garcia <derecerca@gmail.com>
2016-03-19 12:53:22 +0100

    green

--- src/main/java/com/example/etudes/strikeitrich/Game.java
+++ src/main/java/com/example/etudes/strikeitrich/Game.java
@@ -3,15 +3,15 @@ package com.example.etudes.strikeitrich;
 import java.util.List;
 
 public class Game {
-    private final Player player1;
     private final GameStarter gameStarter;
+    private final List<Player> players;
 
     public Game(GameStarter gameStarter, List<Player> players) {
-        this.player1 = players.get(0);
         this.gameStarter = gameStarter;
+        this.players = players;
     }
 
     public void start() {
-        gameStarter.deal(player1);
+        players.forEach(gameStarter::deal);
     }
 }

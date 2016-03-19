5033c887cd0d50da885cb610d052be89f85f570f
Alvaro Garcia <derecerca@gmail.com>
2016-03-19 12:50:07 +0100

    refactor: remove dead code

--- src/main/java/com/example/etudes/strikeitrich/Game.java
+++ src/main/java/com/example/etudes/strikeitrich/Game.java
@@ -1,15 +1,10 @@
 package com.example.etudes.strikeitrich;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Game {
     private final Player player1;
     private final GameStarter gameStarter;
-
-    public Game(Player player1) {
-        this(new GameStarter(), Arrays.asList(player1));
-    }
 
     public Game(GameStarter gameStarter, List<Player> players) {
         this.player1 = players.get(0);

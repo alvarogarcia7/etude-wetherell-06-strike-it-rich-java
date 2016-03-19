5f475cb912a92fac10e0537732162c02c151f1a9
Alvaro Garcia <derecerca@gmail.com>
2016-03-19 15:57:49 +0100

    red: a new turn should make the players pay fixed expenses (remove scaffold)

--- src/test/java/com/example/etudes/strikeitrich/TurnsShould.java
+++ src/test/java/com/example/etudes/strikeitrich/TurnsShould.java
@@ -7,22 +7,25 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TurnsShould {
 
     @Mock
-    private Player player;
+    private Player player1;
+
+    @Mock
+    private Player player2;
 
     @Test
     public void pay_fixed_expenses() throws Exception {
-        Turns turns = new Turns(Arrays.asList(player));
+        Turns turns = new Turns(Arrays.asList(player1, player2));
 
         turns.newTurn();
 
-        verify(player).payFixedExpenses();
+        verify(player1).payFixedExpenses();
+        verify(player2).payFixedExpenses();
     }
 
 

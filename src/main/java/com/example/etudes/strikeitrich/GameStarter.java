56659d892e740d8c28a6d80360110acaadce4462
Alvaro Garcia <derecerca@gmail.com>
2016-03-19 12:54:36 +0100

    refactor: readability

--- src/main/java/com/example/etudes/strikeitrich/GameStarter.java
+++ src/main/java/com/example/etudes/strikeitrich/GameStarter.java
@@ -1,10 +1,10 @@
 package com.example.etudes.strikeitrich;
 
 public class GameStarter {
-    public void deal(Player player1) {
-        player1.receiveStandardFactories(2);
-        player1.receiveRawMaterials(4);
-        player1.receiveFinishedInventoryUnits(2);
-        player1.receiveCash(10_000);
+    public void deal(Player player) {
+        player.receiveStandardFactories(2);
+        player.receiveRawMaterials(4);
+        player.receiveFinishedInventoryUnits(2);
+        player.receiveCash(10_000);
     }
 }

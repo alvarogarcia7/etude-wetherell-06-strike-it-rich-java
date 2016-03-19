18b0d51322cb6effd32a31c6597463bc2aabc9ee
Alvaro Garcia <derecerca@gmail.com>
2016-03-19 12:33:43 +0100

    refactor: inline variable only used once

--- src/test/java/GameShould.java
+++ src/test/java/GameShould.java
@@ -10,11 +10,10 @@ public class GameShould {
     @Test
     public void deal_initial_materials_to_each_player() throws Exception {
         Player player1 = new Player(0, 0, 0, 0);
-        Player expectedPlayer = new Player(2, 4, 2, 10_000);
 
         Game game = new Game(player1);
         game.start();
 
-        assertThat(player1, is(expectedPlayer));
+        assertThat(player1, is(new Player(2, 4, 2, 10_000)));
     }
 }

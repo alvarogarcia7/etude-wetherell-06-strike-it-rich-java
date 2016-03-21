1d78e9f963973dc9d839c343eff31b48143ee15a
Alvaro Garcia <derecerca@gmail.com>
2016-03-21 01:42:43 +0100

    refactor: remove duplication for Act
    #dry #extractMethod

--- src/test/java/com/example/etudes/strikeitrich/BidCalculatorShould.java
+++ src/test/java/com/example/etudes/strikeitrich/BidCalculatorShould.java
@@ -1,14 +1,11 @@
 package com.example.etudes.strikeitrich;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -20,28 +17,19 @@ public class BidCalculatorShould {
 
     @Mock
     private Player player;
-    private Bid bid;
-
-    @Before
-    public void setUp() throws Exception {
-        bid = new Bid(3, 3_000, player);
-    }
-
 
     @Test
     public void accept_the_bid_that_is_over_the_minimum() throws Exception {
-        List<Bid> bids = Arrays.asList(bid);
 
-        new BidCalculator(3, 3_000, bids).distribute();
+        distributeBids(new Bid(3, 3_000, player));
 
         verify(player).acceptBid();
     }
 
     @Test
     public void do_not_accept_bid_below_the_minimum() throws Exception {
-        List<Bid> bids = Arrays.asList(new Bid(1, 2_000, player));
 
-        new BidCalculator(3, 3_000, bids).distribute();
+        distributeBids(new Bid(1, 2_000, player));
 
         verify(player, times(0)).acceptBid();
     }
@@ -49,13 +37,15 @@ public class BidCalculatorShould {
 
     @Test
     public void only_accept_bids_with_positive_requested_units() throws Exception {
-        List<Bid> bids = Arrays.asList(
-                new Bid(0, 4_000, player),
+
+        distributeBids(new Bid(0, 4_000, player),
                 new Bid(-1, 4_000, player));
 
-        new BidCalculator(3, 3_000, bids).distribute();
-
         verify(player, times(0)).acceptBid();
+    }
+
+    private void distributeBids(Bid... bids) {
+        new BidCalculator(3, 3_000, Arrays.asList(bids)).distribute();
     }
 
 

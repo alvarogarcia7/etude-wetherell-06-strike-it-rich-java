bf2cf985bce1f5813438805c1521203cf74104fe
Alvaro Garcia <derecerca@gmail.com>
2016-03-22 18:35:28 +0100

    refactor: remove dead code

--- src/test/java/com/example/etudes/strikeitrich/RawMaterialUnitFixedExpensesShould.java
+++ src/test/java/com/example/etudes/strikeitrich/RawMaterialUnitFixedExpensesShould.java
@@ -6,7 +6,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class RawMaterialUnitFixedExpensesShould extends FixedExpensesShould {
 
     private static final int ANY = 0;
-    private static MaterialsCalculator calculator;
 
     private static final int EACH_UNIT_PRICE = 300;
     private static final int INITIAL_CASH = 10_000;

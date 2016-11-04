9beaf0ce872090d9ffd88ef5a1805439887aa62e
Alvaro Garcia <derecerca@gmail.com>
2016-11-04 20:05:01 +0000

    Refactor: Extract explaining variable, remove duplication

--- src/test/java/com/example/etudes/strikeitrich/PlayerShould.java
+++ src/test/java/com/example/etudes/strikeitrich/PlayerShould.java
@@ -2,24 +2,32 @@ package com.example.etudes.strikeitrich;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PlayerShould {
+
+    @Mock
+    public Condition condition;
+    final Condition condition1FIU_Equals_300Cash = new Condition(
+            (Player p) ->
+            {
+                p.decreaseFinishedInventoryUnits(1);
+                p.receiveCash(300);
+                return null;
+            }
+    );
 
     @Test
     public void exchange_a_single_inventory_item_for_money () {
         Player player = new Player(0, 0, 1, 0, 0);
 
-        player.finishedInventoryUnits(new Condition(
-                (Player p) ->
-                {
-                    p.decreaseFinishedInventoryUnits(1);
-                    p.receiveCash(300);
-                    return null;
-                }
-        ));
+        player.finishedInventoryUnits(condition1FIU_Equals_300Cash);
 
         player.sellInventory();
 
@@ -30,14 +38,7 @@ public class PlayerShould {
     public void exchange_all_inventory_items_for_money () {
         Player player = new Player(0, 0, 2, 0, 0, Strategy.SELL_ALL);
 
-        player.finishedInventoryUnits(new Condition(
-                (Player p) ->
-                {
-                    p.decreaseFinishedInventoryUnits(1);
-                    p.receiveCash(300);
-                    return null;
-                }
-        ));
+        player.finishedInventoryUnits(condition1FIU_Equals_300Cash);
 
         player.sellInventory();
 

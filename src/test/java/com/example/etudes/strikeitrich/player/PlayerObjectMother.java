9143bfff127bbea39119c1c2a7593290cf8356ac
Alvaro Garcia <derecerca@gmail.com>
2017-08-22 21:27:47 +0200

    Add PlayerObjectMother

---
+++ src/test/java/com/example/etudes/strikeitrich/player/PlayerObjectMother.java
@@ -0,0 +1,11 @@
+package com.example.etudes.strikeitrich.player;
+
+import com.example.etudes.strikeitrich.Player;
+
+import java.util.function.IntUnaryOperator;
+
+public class PlayerObjectMother {
+    public static Player sample () {
+        return null;
+    }
+}

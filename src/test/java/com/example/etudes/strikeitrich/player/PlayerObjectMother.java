45f02c1e13ba9d83b6883b4fc83aa868fac04fa7
Alvaro Garcia <derecerca@gmail.com>
2017-09-28 06:10:37 +0200

    Refactor: prefer using the other constructor

--- src/test/java/com/example/etudes/strikeitrich/player/PlayerObjectMother.java
+++ src/test/java/com/example/etudes/strikeitrich/player/PlayerObjectMother.java
@@ -1,6 +1,7 @@
 package com.example.etudes.strikeitrich.player;
 
 import com.example.etudes.strikeitrich.Player;
+import com.example.etudes.strikeitrich.Strategy;
 
 import java.util.function.IntUnaryOperator;
 
@@ -10,6 +11,6 @@ public class PlayerObjectMother {
     }
 
     public static Player emptyWithCash(int cash) {
-        return new Player(0, 0, 0, cash, 0);
+        return new Player(0, 0, 0, cash, 0, Strategy.SELL_ONE, "none");
     }
 }

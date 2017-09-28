549cb5fb48920037346ee898acc2a747f0e7248a
Alvaro Garcia <derecerca@gmail.com>
2017-09-28 06:05:37 +0200

    [3] Refactor: extract method to create the players

--- src/main/java/com/example/etudes/strikeitrich/GameApplication.java
+++ src/main/java/com/example/etudes/strikeitrich/GameApplication.java
@@ -7,11 +7,15 @@ import static com.example.etudes.strikeitrich.Strategy.SELL_ONE;
 
 public class GameApplication {
     public static void main(String[] args) {
-        Player p1 = new Player(0,0,0,0,0, SELL_ONE, "P1");
-        Player p2 = new Player(0,0,0,0,0, SELL_ONE, "P2");
+        Player p1 = emptyPlayerWithName("P1");
+        Player p2 = emptyPlayerWithName("P2");
         final List<Player> playerList = Arrays.asList(p1, p2);
         Game game = new Game(new GameStarter(), Turns.aNew(playerList, new Bank(), MaterialsCalculator.defaultPrices()), playerList);
 
         game.start();
+    }
+
+    private static Player emptyPlayerWithName(String p1) {
+        return new Player(0, 0, 0, 0, 0, SELL_ONE, p1);
     }
 }
